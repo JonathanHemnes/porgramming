39e6d0042c75ad77022d9cb46d1f17421222a567
Jonathan Hemnes <jhemnes+github@gmail.com>
2019-02-09 09:07:07 -0700

    upgrades

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Porgramming`,
+    author: `Jonathan Hemnes`,
+    description: `Solutions to problems I've encountered`,
+    siteUrl: `https://www.porgramming.com`,
     social: {
       twitter: `kylemathews`,
     },
--- src/components/bio.js
+++ src/components/bio.js
@@ -31,12 +31,9 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong> who lives and works in
+              Denver building useful things.
               {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
-              </a>
             </p>
           </div>
         )
